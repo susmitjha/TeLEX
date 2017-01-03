722125730d8223b2257fe19b354e281f6e987eac
Susmit Jha <jha@banshee.csl.sri.com>
2017-01-02 16:28:43 -0800

    Structuring sources

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+EXCLUDE_FROM_PACKAGES = ["tests","build","docs"]
+
+setup(name='TeLEX',
+      version = '0.1',
+      description = 'Temporal Logic Extractor',
+      author = 'Susmit Jha',
+      author_email = 'jha@eecs.berkeley.edu',
+      license = '',
+      install_requires=[
+          'parsimonious',
+          'numpy',
+          'pandas'
+      ],
+      packages=find_packages(),
+)
---
+++ telex/__init__.py
@@ -0,0 +1,5 @@
+from telex.stl import *
+from telex.inputreader import *
+from parametrizer import *
+from scorer import *
+from synth import *
