968cd82000ff639ae92ccf7b4bc0d47d9c52457f
Susmit <susmitjha>
2017-01-22 02:32:43 -0800

    added optimizer option

--- tests/test_scale.py
+++ tests/test_scale.py
@@ -10,24 +10,24 @@ import sys, getopt
 templogicdata =  [
     'G[0,1](x <= a? 0;12 & x >= b? -12;0 )',
     '(  G[0,1](x <= a? 0;12 & x >= b? -12;0 )  &  G[1,2](x <= c? 0;12 & x >= d? -12;0 ) )',
-    '( (  G[0,1](x <= a? 0;12 & x >= b? -12;0 )  &  G[1,2](x <= c? 0;12 & x >= d? -12;0 ) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0 )  ) ',
-    '( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) )',
-    '( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) )',
-    '( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) )',
-    '( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) )',
-    '( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) )',
-    '( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) )',
-    '( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) )',
-    '( ( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) ) & G[10,11](x <= i? 0;12 & x >= j? -12;0) )',
-    '( ( ( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) ) & G[10,11](x <= i? 0;12 & x >= j? -12;0) ) & G[11,12](x <= i? 0;12 & x >= j? -12;0) )',
+#    '( (  G[0,1](x <= a? 0;12 & x >= b? -12;0 )  &  G[1,2](x <= c? 0;12 & x >= d? -12;0 ) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0 )  ) ',
+#    '( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) )',
+#    '( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) )',
+#    '( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) )',
+#    '( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) )',
+#    '( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) )',
+#    '( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) )',
+#    '( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) )',
+#    '( ( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) ) & G[10,11](x <= i? 0;12 & x >= j? -12;0) )',
+#    '( ( ( ( ( ( ( ( ( ( (  G[0,1](x <= a? 0;12 & x >= b? -12;0)  &  G[1,2](x <= c? 0;12 & x >= d? -12;0) )  &  G[2,3](x <= e? 0;12 & x >= f? -12;0)  ) & G[3,4](x <= g? 0;12 & x >= h? -12;0) ) & G[4,5](x <= i? 0;12 & x >= j? -12;0) ) & G[5,6](x <= k? 0;12 & x >= l? -12;0) ) & G[6,7](x <= m? 0;12 & x >= n? -12;0) ) & G[7,8](x <= o? 0;12 & x >= p? -12;0) ) & G[8,9](x <= q? 0;12 & x >= r? -12;0) ) & G[9,10](x <= s? 0;12 & x >= t? -12;0) ) & G[10,11](x <= i? 0;12 & x >= j? -12;0) ) & G[11,12](x <= i? 0;12 & x >= j? -12;0) )',
 ]
 
 #G[4,5](x <= i? 0;10 & x >= j? -10;0)
 
 @pytest.mark.parametrize("tlStr", templogicdata)
-def test_stl(tlStr):
+def test_stl(tlStr, optmethod = "L-BFGS-B"):
     print(tlStr)
-    (stlsyn, value, dur) = telex.synth.synthSTLParam(tlStr, "scale","L-BFGS-B")
+    (stlsyn, value, dur) = telex.synth.synthSTLParam(tlStr, "scale", optmethod)
     print(" Synthesized STL formula: {}\n Theta Optimal Value: {}\n Optimization time: {}\n".format(stlsyn, value, dur))
     (bres, qres) = telex.synth.verifySTL(stlsyn, "scale")
     print(" Test result of synthesized STL on each trace: {}\n Robustness Metric Value: {}\n".format(bres, qres))
@@ -36,42 +36,50 @@ def test_stl(tlStr):
 
 def main(argv):
     itercount = 2
+    optmethod = "L-BFGS-B"
     try: 
-        opts,args = getopt.getopt(argv, "hi:",["itercount="])
+        opts,args = getopt.getopt(argv, "hi:o:",["itercount=","optmethod="])
     except getopt.GetoptError:
-        print 'python test_scale.py -i <number of times to iterate each synthesis task to compute mean runtime>'
+        print 'python test_scale.py -i <number of times to iterate each synthesis task to compute mean runtime> -o <opt-method>' 
         sys.exit(2)
     for opt,arg in opts:
         if opt == '-h':
-            print 'python test_scale.py -i <number of times to iterate each synthesis task to compute mean runtime>'
+            print 'python test_scale.py -i <number of times to iterate each synthesis task to compute mean runtime> -o <opt-method>'
+            print 'Valid opt-methods: \"L-BFGS-B\",\"TNC\", \"SLSQP"\, \"DE\"'
             sys.exit()
         elif opt in ("-i", "--itercount"):
             itercount = int(arg)
+        elif opt in ("-o", "--optmethod"):
+            optmethod = arg
 
     runtime = {}
-    rhovalues = []
+    rhovalues = {}
 
     for templ in templogicdata:
         runtime[templ] = []
+        rhovalues[templ] = []
 
     for i in range(0,itercount):
         for templ in templogicdata:
-            value, dur = test_stl(templ)
+            value, dur = test_stl(templ, optmethod)
             runtime[templ].append(dur)
-            rhovalues.append(value)
-
+            rhovalues[templ].append(value)
 
     print("======================================================================")
-    template = "{0:5}|{1:10}|{2:25}|{3:25}" 
-    print template.format("ID", " #Params", "     Mean Runtime", "     Variance in Runtime")
+    print("               Optmethod {}".format(optmethod))
+    print("======================================================================")
+    print("                Averaging over {} Iterations".format(itercount))
+    print("======================================================================")
+    template = "{0:5}|{1:10}|{2:25}|{3:25}|{4:20}|{5:20}" 
+    print template.format("ID", " #Params", "     Mean Runtime", "     Variance in Runtime", "  Rho Mean ", " Rho Var ")
     print("----------------------------------------------------------------------")
     i = 1
     for templ in templogicdata:
-        print(template.format(i, 2*i, numpy.mean(runtime[templ]), numpy.var(runtime[templ])))
+        print(template.format(i, 2*i, numpy.mean(runtime[templ]), numpy.var(runtime[templ]), numpy.mean(rhovalues[templ]), numpy.var(rhovalues[templ]) ))
         i = i+1
         print("----------------------------------------------------------------------")
     print("======================================================================")
-    print("Mean Robustness Value: {}, Variance: {}".format(numpy.mean(rhovalues), numpy.var(rhovalues) ) )
+    #print("Mean Robustness Value: {}, Variance: {}".format(numpy.mean(rhovalues), numpy.var(rhovalues) ) )
 
 
 if __name__ == "__main__":
